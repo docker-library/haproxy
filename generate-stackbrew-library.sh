c16bfc80cd2e92758c791b97ad802cd62f099203
Tianon Gravi <admwiggin@gmail.com>
2014-11-25 21:54:54 -0700

    GENERATE ALL THE THINGS

---
+++ generate-stackbrew-library.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+declare -A aliases
+aliases=(
+	[1.5]='1 latest'
+)
+
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+
+versions=( */ )
+versions=( "${versions[@]%/}" )
+url='git://github.com/docker-library/haproxy'
+
+echo '# maintainer: InfoSiftr <github@infosiftr.com> (@infosiftr)'
+
+for version in "${versions[@]}"; do
+	commit="$(git log -1 --format='format:%H' -- "$version")"
+	fullVersion="$(grep -m1 'ENV HAPROXY_VERSION ' "$version/Dockerfile" | cut -d' ' -f3)"
+	versionAliases=( $fullVersion $version ${aliases[$version]} )
+	
+	echo
+	for va in "${versionAliases[@]}"; do
+		echo "$va: ${url}@${commit} $version"
+	done
+done
