f0d2bb10b36374315e67dfe317ef29119e778f25
Kristian Klausen <gitlab.busk@riseup.net>
2016-03-06 20:09:47 +0100

    Add reload support
    Fix #5

---
+++ 1.5/docker-entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -eo pipefail
+
+# first arg is `-f` or `--some-option`
+if [ "${1:0:1}" = '-' ]; then
+	set -- haproxy "$@"
+fi
+
+if [ "$1" = 'haproxy' ]; then
+	# if the user wants "haproxy", let's use "haproxy-systemd-wrapper" instead so we can have proper reloadability implemented by upstream
+	shift # "haproxy"
+	set -- "$(which haproxy-systemd-wrapper)" -p /run/haproxy.pid "$@"
+fi
+
+exec "$@"
